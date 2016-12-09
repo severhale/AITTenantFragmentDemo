09605f9383a8c7c1ec555943b04eab003f6372c8
sgrund14 <sgrund@oberlin.edu>
2016-12-09 02:21:09 +0100

    connection recycler adapter added

--- app/src/main/java/samandsimons/adventure/aittenantfragmentdemo/adapter/ConnectionRecyclerAdapter.java
+++ app/src/main/java/samandsimons/adventure/aittenantfragmentdemo/adapter/ConnectionRecyclerAdapter.java
@@ -8,6 +8,11 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -48,29 +53,52 @@ public class ConnectionRecyclerAdapter extends RecyclerView.Adapter<ConnectionRe
     }
 
     @Override
-    public void onBindViewHolder(ConnectionRecyclerAdapter.ViewHolder holder, int position) {
-        Connection connection = connectionList.get(position);
+    public void onBindViewHolder(final ConnectionRecyclerAdapter.ViewHolder holder, final int position) {
+        final Connection connection = connectionList.get(position);
 
         holder.name.setText(connection.getDisplayName());
 
         holder.btnConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                connection.setConfirmed(true);
+                notifyItemChanged(position);
             }
         });
 
         holder.btnDeny.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                connection.setConfirmed(false);
+                notifyItemChanged(position);
             }
         });
 
     }
 
+    public void addConnection(String emailAddress) {
+        FirebaseDatabase.getInstance().getReference().child("user").
+                orderByChild("emailAddress").equalTo(emailAddress)
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        User user = dataSnapshot.getValue(User.class);
+                        String displayName = user.getEmail();
+                        String uid = dataSnapshot.getKey();
+
+                        Connection newConnection = new Connection(false, uid, displayName);
+                        connectionList.add(newConnection);
+                        notifyItemInserted(connectionList.size()-1);
+                    }
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        Log.d("FIREBASE_CANCELLED", databaseError.getMessage());
+                    }
+                });
+    }
+
     public void updateForUser(User user) {
-        Log.d("TAG", "Updating for user with " + user.getPayments().size() + " payments");
+        Log.d("TAG", "Updating for user with " + user.getConnections().size() + " connections.");
 
         connectionList = user.getConnections();
 
@@ -79,6 +107,6 @@ public class ConnectionRecyclerAdapter extends RecyclerView.Adapter<ConnectionRe
 
     @Override
     public int getItemCount() {
-        return 0;
+        return connectionList.size();
     }
 }
---
+++ app/src/main/java/samandsimons/adventure/aittenantfragmentdemo/fragment/AddConnectionDialogFragment.java
@@ -0,0 +1,51 @@
+package samandsimons.adventure.aittenantfragmentdemo.fragment;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by samgrund on 12/9/16.
+ */
+public class AddConnectionDialogFragment extends DialogFragment {
+
+
+    public AddConnectionDialogFragment() {
+    }
+
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
+
+        alertDialogBuilder.setTitle("Add new connection.");
+
+        alertDialogBuilder.setPositiveButton("Connect", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+
+
+            }
+        });
+        alertDialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+
+
+
+        return alertDialogBuilder.create();
+    }
+}
